56ddb6865111b224814c6083cccf2869a87d32ac
703Pvrtcx <kasaokapapkies@gmail.com>
2021-06-30 16:10:45 +0200

    Added Desktop login page, without media queries

--- index.php
+++ index.php
@@ -8,54 +8,7 @@
   <link href="style.css" rel="stylesheet"/>
 </head>
 <body>
-<!-- <?php
-    //  echo "
-    //  <div id="login-container"> 
-    //        <section>      
-    //             <div class="row">
-    //                 <div class="col">
-    //                 <form action="/action_page.php">
-    //                     <div class="input-container">
-    //                         <i class="fa fa-user icon"></i>
-    //                         <input class="input-field" type="text" placeholder="Username" name="username">
-    //                     </div>
-    //                     <div class="input-container">
-    //                        <i class="fa fa-lock icon"></i>
-    //                         <input class="input-field" type="password" placeholder="Password" name="password">
-    //                     </div>
-                      
-    //                     <div>
-    //                     <label>
-                               
-    //                     Forgot password?
-                        
-    //                     </label>
-    //                     <button  type="submit" class="btn">
-    //                        SIGN IN
-    //                     </button>
-    //                     </div>
-    //                     </form>
-    //                     </div>
-
-
-    //               <div class="line">
-    //                 </div>
-                    
-                    
-    //                 <div class="col">
-                      
-    //                 <div class="logo">
-    //                 LOGO
-    //                 </div>
-
-    //                 </div>
-    //             </div>
-    //        </section>
-    //  </div>      
-    //  ";
-    ?> -->
-
-<div id="login-container"> 
+<!-- <div id="login-container"> 
            <section>      
                 <div class="row">
                     <div class="col">
@@ -96,7 +49,42 @@
                     </div>
                 </div>
            </section>
-     </div> 
+     </div>  -->
+
+
+     <div class="container">
+            <div class="sign-in">  
+                <form action="/action_page.php" style="max-width:500px;margin:auto">
+                    
+                    <div class="input-container">
+                      <i class="fa fa-user icon"></i>
+                      <input class="input-field" type="text" placeholder="Username" name="usrnm">
+                    </div>
+                    
+                    <div class="input-container">
+                      <i class="fa fa-lock icon"></i>
+                      <input class="input-field" type="password" placeholder="Password" name="psw">
+                    </div>
+                  
+                    <div class="bottom-container">    
+                        <span class="forget-password">
+                            <a href="">
+                                Forgot password?
+                            </a>
+                        </span>
+                        <button  type="submit" class="btn">
+                            SIGN IN
+                        </button>
+                    </div>
+                  
+                </form>
+            </div>
+            <div class="vertical-line">  
+            </div>
+            <div class="logo">
+                LOGO
+            </div>
+        </div>
 
 
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
